930c19e86ec149a8679e106a494dd1172df9e125
Matthieu Luiggi <matthieu.luiggi@saint-benigne.fr>
2025-09-28 23:47:57 +0200

    Activation du fichier CSS d'authentification dans la configuration de Vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
-                // 'resources/css/auth.css',
+                'resources/css/auth.css',
             ],
             refresh: true,
         }),
